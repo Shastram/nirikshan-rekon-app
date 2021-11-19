d0e2bf6dd7639e4ae7838321c9fbd2cf78ace853
Hemanth Krishna <hkpdev008@gmail.com>
2021-11-19 12:26:51 +0530

    add: home initial ui
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({Key? key}) : super(key: key);
@@ -14,14 +15,16 @@ class _HomeWidgetState extends State<HomeWidget> {
       body: SafeArea(
         child: Stack(
           children: [
-            const Text("hello"),
+            _header(),
+            _card(),
             SizedBox.expand(
               child: DraggableScrollableSheet(
-                maxChildSize: 0.8,
+                maxChildSize: 0.97,
                 minChildSize: 0.5,
                 builder:
                     (BuildContext context, ScrollController scrollController) {
                   return Container(
+                    padding: const EdgeInsets.only(top: 32),
                     decoration: const BoxDecoration(
                       color: Colors.blue,
                       borderRadius: BorderRadius.only(
@@ -29,13 +32,7 @@ class _HomeWidgetState extends State<HomeWidget> {
                         topRight: Radius.circular(20),
                       ),
                     ),
-                    child: ListView.builder(
-                      controller: scrollController,
-                      itemCount: 25,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(title: Text('Item $index'));
-                      },
-                    ),
+                    child: _buildList(scrollController),
                   );
                 },
               ),
@@ -45,4 +42,86 @@ class _HomeWidgetState extends State<HomeWidget> {
       ),
     );
   }
+
+  Widget _buildList(ScrollController scrollController) {
+    return ListView.builder(
+      itemCount: 25,
+      controller: scrollController,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text("Time"),
+                  Text("Device"),
+                  Text("Blacklist"),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _header() {
+    return Container(
+      padding: const EdgeInsets.only(top: 32),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: const Text(
+              "Hello User",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _card() {
+    final List<ChartData> chartData = [
+      ChartData('David', 25),
+      ChartData('Steve', 38),
+      ChartData('Jack', 34),
+      ChartData('Others', 52)
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 64),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: SizedBox(
+          height: 150,
+          child: Center(
+            child: SfCircularChart(
+              legend: Legend(isVisible: true),
+              series: <CircularSeries>[
+                // Render pie chart
+                PieSeries<ChartData, String>(
+                    dataSource: chartData,
+                    xValueMapper: (ChartData data, _) => data.x,
+                    yValueMapper: (ChartData data, _) => data.y)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
 }
